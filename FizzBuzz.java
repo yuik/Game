677df0de93bce184a869c79f2176e882fae9003f
ttbe <dai06suke27@gmail.com>
2014-10-02 23:01:44 +0900

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		for (int i =1; i < 30; i++){
+			if(i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
+			else if(i % 3 == 0) System.out.println("Fizz");
+			else if(i % 5 == 0) System.out.println("Buzz");
+			else System.out.println(i);
+			
+		}
+	}
+
+}
